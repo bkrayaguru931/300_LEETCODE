2ca4c09c491f6d3100c9961d1ed8627198e0fc46
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2022-08-17 16:25:52 +0530

    Create Leet0804.cpp

---
+++ Leet0804.cpp
@@ -0,0 +1,26 @@
+//804. Unique Morse Code Words
+
+
+
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> t = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        // insert to set so that we can find unique solutions
+        set<string> s;
+
+        for(int i = 0; i < words.size();i++){
+            string tt = words[i]; // Present word
+            string temp = ""; // to store coded word from tt
+
+            for(int j= 0 ;j < tt.length(); j++){
+                int e = tt[j] - 'a'; // we get the particular index
+                temp += t[e];
+            }
+            s.insert(temp); // insert all codes
+        }
+        return s.size(); 
+    }
+};
