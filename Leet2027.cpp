aaa3425662e76ec2888b296bda0ee819faceb862
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2022-11-30 09:49:51 +0530

    Create Leet2027.cpp

---
+++ Leet2027.cpp
@@ -0,0 +1,18 @@
+//2027. Minimum Moves to Convert String
+
+
+
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int n = s.size();
+		int ans = 0;
+		for(int i=0;i<n;i++){
+			if(s[i] == 'X'){
+				ans++;
+				i+=2;
+			}
+		}
+		return ans;
+    }
+};
