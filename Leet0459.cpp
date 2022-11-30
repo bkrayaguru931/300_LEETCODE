2b52c9c0e740813aadd69e0c600849f44524eba4
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2022-11-30 14:04:35 +0530

    Create Leet0459.cpp

---
+++ Leet0459.cpp
@@ -0,0 +1,27 @@
+//459. Repeated Substring Pattern
+
+
+
+class Solution {
+public:
+    bool issafe(string s,string str){
+        for(int i=0;i<s.length();i+=str.length()){
+            string cs=s.substr(i,str.length());
+            if(cs!=str) return false;
+        }
+        return true;
+    }	
+	
+    bool repeatedSubstringPattern(string s) {
+         string str="";
+        for(int i=0;i<s.length()/2;i++){
+            str+=s[i];
+            if(s.length()%str.length()==0){
+                int x=issafe(s.substr(i+1),str);
+                if(x) return true;
+            }
+            
+	    }
+        return false;
+    }
+};
