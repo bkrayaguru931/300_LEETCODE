1cfc6ae3863b0dad810096ae82b73d1358564960
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2022-12-02 14:56:59 +0530

    Create Leet0904.cpp

---
+++ Leet0904.cpp
@@ -0,0 +1,27 @@
+//904. Fruit Into Baskets
+
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int i=0,j=0;
+        int maxi=1;
+        unordered_map<int,int> mp;
+        while (j<fruits.size())
+        {
+            mp[fruits[j]]++;
+            while (mp.size()>2)
+            {
+                mp[fruits[i]]--;
+                if (mp[fruits[i]]==0)
+                    mp.erase(fruits[i]);
+                i++;
+            }
+            if (mp.size()<=2)
+                maxi=max(maxi,j-i+1);
+            j++;
+        }
+        return maxi;
+    }
+};
